b494a21ea3ffbf7b502ebcfa0529bfd209984797
Michal Ciesla <ciesla.m@outlook.com>
2025-12-04 13:55:26 +0100

    Change output pins to fit the current board

--- HoloControl-Pico/control_globals.c
+++ HoloControl-Pico/control_globals.c
@@ -3,5 +3,5 @@
 #include "colors.h"
 
 char G_mode = MODE_OFF, G_prev_mode = MODE_OFF, G_color = COLOR_OFF, G_white = 0, G_auto_run = 0;
-long unsigned int G_exposures[3] = {0, 0, 0};
+long unsigned int G_exposures[4] = {0, 0, 0, 0};
 long unsigned int G_finishing = 0, G_waiting = 0;
