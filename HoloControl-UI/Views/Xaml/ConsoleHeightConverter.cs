f58268399d890e9dcfedc62492ae6d4da3ae9809
Michal Ciesla <ciesla.m@outlook.com>
2025-12-05 21:55:15 +0100

    Fix #1, set height of console output window, change sending indicator, keep history scrolled to the end

---
+++ HoloControl-UI/Views/Xaml/ConsoleHeightConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace HoloControl.Views.Xaml
+{
+    class ConsoleHeightConverter : IValueConverter
+    {
+        const double HEIGHT_OFFSET = 160;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double height) return height - HEIGHT_OFFSET;
+            else throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double height) return height + HEIGHT_OFFSET;
+            else throw new NotImplementedException();
+        }
+    }
+}
